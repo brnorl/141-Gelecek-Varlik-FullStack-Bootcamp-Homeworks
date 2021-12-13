5550f51a2178d3b0d071dda626a396dc74839acc
Baransel Oral <52318753+brnorl@users.noreply.github.com>
2021-12-13 16:30:12 +0300

    homework-3 added.

---
+++ bootcamp-3/odev3.DB/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+#nullable disable
+
+namespace odev3.DB.Models
+{
+    public partial class Product
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public decimal Price { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
